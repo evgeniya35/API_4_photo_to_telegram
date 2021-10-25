65fcbd3c42a5e5dbcee695d540bcd233d4594823
evgeniya35 <evgeniya35@yandex.ru>
2021-10-25 21:14:43 +0300

    Developed a script load_photo.py

--- load_photo.py
+++ load_photo.py
@@ -1,11 +1,20 @@
 import os
 import requests
 
-path_photo = "media\photo\hubble.jpeg"
-filename = os.path.join(os.getcwd(), path_photo)
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-response = requests.get(url=url)
-response.raise_for_status()
-with open(filename, mode="wb") as file:
-    file.write(response.content)
+def loadphoto(url, folder, photo):
+    if not os.path.exists(os.path.join(os.getcwd(), folder)):
+        os.makedirs(os.path.join(os.getcwd(), folder))
+    filename = os.path.join(os.getcwd(), folder, photo)
+    headers = {"user-agent": "CoolTool / 0.0"}
+    response = requests.get(url=url, headers=headers)
+    response.raise_for_status()
+    with open(filename, mode="wb") as file:
+        file.write(response.content)
 
+
+if __name__ == "__main__":       
+    loadphoto(
+        url="https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg",
+        folder="images",
+        photo="hubble.jpeg"
+        )
