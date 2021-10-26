f385d88914664dffed38700fbb0c57d078ff886a
evgeniya35 <evgeniya35@yandex.ru>
2021-10-26 21:28:08 +0300

    Add function fetch_nasa_photo

--- photo_spacex.py
+++ photo_spacex.py
@@ -1,6 +1,12 @@
-from os import environ
+import os
 import requests
 import json
+
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from urllib.parse import urlparse, unquote
+
+from requests.sessions import extract_cookies_to_jar
 
 from load_photo import loadphoto
 
@@ -11,12 +17,44 @@ def fetch_spacex_last_launch(url):
     return response.json()["links"]["flickr"]["original"]
 
 
-if __name__ == "__main__":
+def photo_ext(url):
+    url_components = urlparse(unquote(url, encoding="utf-8"))
+    return os.path.splitext(url_components.path)[-1]
+
+
+def fetch_nasa_photo(days=3):
+    load_dotenv()
+    options = {
+        "api_key": os.environ.get("NASA_API_KEY"),
+        "start_date": (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d"),
+        "end_date": datetime.now().strftime("%Y-%m-%d")
+    } 
+    url = "https://api.nasa.gov/planetary/apod"
+    response = requests.get(url=url, params=options)
+    response.raise_for_status()
+    for num, photo in enumerate(response.json()):
+        if photo["media_type"] == "image":
+            loadphoto(
+                photo["url"],
+                r"images\nasa",
+                f"nasa{num}{photo_ext(photo['url'])}"
+            )
+    
+
+def main():
+    fetch_nasa_photo(days=4)
+    
     url = "https://api.spacexdata.com/v5/launches/5eb87d46ffd86e000604b388"
     photos = fetch_spacex_last_launch(url=url)
     for photo_num, url in enumerate(photos):
         loadphoto(
             url=url,
-            folder="images",
+            folder="images\spacex",
             photo=f"spacex{photo_num}.jpg"
             )
+
+
+if __name__ == "__main__":
+     main()
+
+    
