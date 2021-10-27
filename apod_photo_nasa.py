5bb38f9706b3d380e54d3c42d7a881625bb2098a
evgeniya35 <evgeniya35@yandex.ru>
2021-10-27 21:21:32 +0300

    Change photo_nasa.py -> apod_photo_nasa.py

--- apod_photo_nasa.py
+++ apod_photo_nasa.py
@@ -4,14 +4,8 @@ import json
 
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-from urllib.parse import urlparse, unquote
 
-from load_photo import loadphoto
-
-
-def photo_ext(url):
-    url_components = urlparse(unquote(url, encoding="utf-8"))
-    return os.path.splitext(url_components.path)[-1]
+from load_photo import loadphoto, photo_ext
 
 
 def fetch_nasa_photo(days=3):
@@ -20,7 +14,7 @@ def fetch_nasa_photo(days=3):
         "api_key": os.environ.get("NASA_API_KEY"),
         "start_date": (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d"),
         "end_date": datetime.now().strftime("%Y-%m-%d")
-    } 
+    }
     url = "https://api.nasa.gov/planetary/apod"
     response = requests.get(url=url, params=options)
     response.raise_for_status()
@@ -31,11 +25,11 @@ def fetch_nasa_photo(days=3):
                 r"images\nasa",
                 f"nasa{num}{photo_ext(photo['url'])}"
             )
-    
+
 
 def main():
     fetch_nasa_photo(days=4)
 
 
 if __name__ == "__main__":
-     main()
+    main()
