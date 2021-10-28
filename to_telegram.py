0a8a1f97b1f357d8fa5afa5d25c7615516a27ff9
evgeniya35 <evgeniya35@yandex.ru>
2021-10-28 21:54:24 +0300

    Add script for publishing photo

--- to_telegram.py
+++ to_telegram.py
@@ -1,13 +1,24 @@
 import os
-
-from dotenv import load_dotenv
+import time
 import telegram
 
+from dotenv import load_dotenv
 
-load_dotenv()
-telegram_token = os.environ.get("TELEGRAM_TOKEN")
-bot = telegram.Bot(token=telegram_token)
-bot.send_document(
-    chat_id="-1001730774444",
-    document=open(file="images/hubble.jpeg", mode="rb")
-    )
+
+def pub_image(delay=86400):
+    telegram_token = os.environ.get("TELEGRAM_TOKEN")
+    bot = telegram.Bot(token=telegram_token)
+    while True:
+        for dpath, dirnames, fnames in os.walk("images"):
+            for fname in fnames:
+                bot.send_document(
+                    chat_id="-1001730774444",
+                    document=open(file=os.path.join(dpath, fname), mode="rb")
+                    )
+        time.sleep(delay)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    delay = int(os.environ.get("PUBLISH_DELAY"))
+    pub_image(delay)
