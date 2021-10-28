82b917e1eeeaa9a808df7dea3c5291b2862d140d
evgeniya35 <evgeniya35@yandex.ru>
2021-10-28 19:40:31 +0300

    publish photo to Telegram channel

--- to_telegram.py
+++ to_telegram.py
@@ -7,8 +7,7 @@ import telegram
 load_dotenv()
 telegram_token = os.environ.get("TELEGRAM_TOKEN")
 bot = telegram.Bot(token=telegram_token)
-bot.send_message(
+bot.send_document(
     chat_id="-1001730774444",
-    text="Text from python-telegram-bot script"
+    document=open(file="images/hubble.jpeg", mode="rb")
     )
-
