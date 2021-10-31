67efb2e75ee65d6ee17d2e530c9c49456b124498
evgeniya35 <evgeniya35@yandex.ru>
2021-10-31 18:58:51 +0300

    movied url to etch_spacex_launch

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,15 +5,15 @@ import json
 from load_photo import load_photo, create_folder
 
 
-def fetch_spacex_last_launch(url):
+def fetch_spacex_launch(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url=url)
     response.raise_for_status()
     return response.json()["links"]["flickr"]["original"]
 
 
 def main():
-    url = "https://api.spacexdata.com/v5/launches/5eb87d46ffd86e000604b388"
-    photos = fetch_spacex_last_launch(url=url)
+    photos = fetch_spacex_launch(launch_id="5eb87d46ffd86e000604b388")
     create_folder(os.path.join(os.getcwd(), "images", "spacex"))
     for photo_num, url in enumerate(photos):
         load_photo(
