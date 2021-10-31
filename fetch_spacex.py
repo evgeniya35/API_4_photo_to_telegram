eb1ccbb91d14fd83e170d810726fda997a1770e3
evgeniya35 <evgeniya35@yandex.ru>
2021-10-31 17:54:49 +0300

    Removed loading .env from functions

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 
-from load_photo import loadphoto
+from load_photo import load_photo, create_folder
 
 
 def fetch_spacex_last_launch(url):
@@ -14,11 +14,11 @@ def fetch_spacex_last_launch(url):
 def main():
     url = "https://api.spacexdata.com/v5/launches/5eb87d46ffd86e000604b388"
     photos = fetch_spacex_last_launch(url=url)
+    create_folder(os.path.join(os.getcwd(), "images", "spacex"))
     for photo_num, url in enumerate(photos):
-        loadphoto(
+        load_photo(
             url=url,
-            folder="images\spacex",
-            photo=f"spacex{photo_num}.jpg"
+            file_name=os.path.join(os.getcwd(), "images", "spacex", f"spacex{photo_num}.jpg")
             )
 
 
--- to_telegram.py
+++ to_telegram.py
@@ -5,10 +5,7 @@ import telegram
 from dotenv import load_dotenv
 
 
-def pub_image():
-    telegram_token = os.environ.get("TELEGRAM_TOKEN")
-    delay = int(os.environ.get("PUBLISH_DELAY", 86400))
-    telegram_channel = os.environ.get("TELEGRAM_CHANNEL")
+def pub_image(telegram_token, telegram_channel, delay=86400):
     bot = telegram.Bot(token=telegram_token)
     while True:
         for dir_path, dir_names, file_names in os.walk("images"):
@@ -17,9 +14,13 @@ def pub_image():
                     chat_id=telegram_channel,
                     document=open(file=os.path.join(dir_path, file_name), mode="rb")
                     )
-        time.sleep(delay)
+            time.sleep(delay)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    pub_image()
+    telegram_token = os.environ.get("TELEGRAM_TOKEN")
+    telegram_channel = os.environ.get("TELEGRAM_CHANNEL")
+    delay = int(os.environ.get("PUBLISH_DELAY"))
+    pub_image(telegram_token, telegram_channel, delay)
+ 
