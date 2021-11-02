0d1e843995fb6c3d78ef5da3c54b679487d162e6
evgeniya35 <evgeniya35@yandex.ru>
2021-11-02 08:36:45 +0300

    add argument `folder` in `pub_images`

--- to_telegram.py
+++ to_telegram.py
@@ -5,10 +5,10 @@ import telegram
 from dotenv import load_dotenv
 
 
-def pub_image(telegram_token, telegram_channel, delay=86400):
+def pub_image(telegram_token, telegram_channel, delay=86400, folder="images"):
     bot = telegram.Bot(token=telegram_token)
     while True:
-        for dir_path, dir_names, file_names in os.walk("images"):
+        for dir_path, dir_names, file_names in os.walk(folder):
             for file_name in file_names:
                 with open(file=os.path.join(dir_path, file_name), mode="rb") as f:
                     doc = f.read()
@@ -24,7 +24,7 @@ def main():
     telegram_token = os.environ.get("TELEGRAM_TOKEN")
     telegram_channel = os.environ.get("TELEGRAM_CHANNEL")
     delay = int(os.environ.get("PUBLISH_DELAY"))
-    pub_image(telegram_token, telegram_channel, delay)
+    pub_image(telegram_token, telegram_channel, delay, "images")
 
 
 if __name__ == "__main__":
